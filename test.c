b4a1bf5170ebabd1c463911857446ef78ed0ef1a
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-11-13 04:14:24 +0200

    Task 0 - 6th attempt to fix all errors

---
+++ test.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - checks the code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	_printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	_printf("%%\n");
+	return (0);
+}
