dd238a9cdb0ba9f6b30e21ff1ff6a1f6fbce2f86
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-11-14 18:03:13 +0200

    TAsk 0 - error 0n 18-8

--- _printf.c
+++ _printf.c
@@ -33,13 +33,8 @@ int _printf(const char *format, ...)
 				char_cnt += _putchar('%');
 			else
 			{
-				if (*format >= 32 && *format < 127)
-				{
-					char_cnt += _putchar('%');
-					char_cnt += _putchar(*format);
-				}
-				else
-					char_cnt += _print_str("error");
+				char_cnt += _putchar('%');
+				char_cnt += _putchar(*format);
 			}
 		}
 		format++;
