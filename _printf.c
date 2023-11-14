9ba9f26c2d14d79f10ee173041fcb3a41bf22dec
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-11-14 18:44:02 +0200

    TAsk 0 - error 0n 18-13

--- _printf.c
+++ _printf.c
@@ -11,7 +11,7 @@ int _printf(const char *format, ...)
 	int char_cnt = 0;
 	va_list args_list;
 
-	if (format == NULL || (*format == '%' && *(format + 1) == '\0'))
+	if (format == NULL || (*format == '%' && *(format + 1) == '\0') || *format == "")
 		return (-1);
 	va_start(args_list, format);
 	while (*format != '\0')
